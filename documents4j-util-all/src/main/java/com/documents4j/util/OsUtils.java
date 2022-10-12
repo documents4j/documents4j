9feb902f9e336405d0bb0cdf0bcf5106b0f04a62
Jan-Patrick Osterloh <jposterloh@web.de>
2022-10-12 13:22:35 +0200

    Added Utitlities for OS Detection

---
+++ documents4j-util-all/src/main/java/com/documents4j/util/OsUtils.java
@@ -0,0 +1,27 @@
+package com.documents4j.util;
+
+public class OsUtils {
+
+    private static String myOS = System.getProperty("os.name").toLowerCase();
+
+    private OsUtils() {
+        /* empty, but suppress visibility outside of package */
+    }
+
+    public static boolean isWindows() {
+        return (myOS.indexOf("win") >= 0);
+    }
+
+    public static boolean isMac() {
+        return (myOS.indexOf("mac") >= 0);
+    }
+
+    public static boolean isUnix() {
+        return (myOS.indexOf("nix") >= 0 || myOS.indexOf("nux") >= 0 || myOS.indexOf("aix") > 0);
+    }
+
+    public static boolean isSolaris() {
+        return (myOS.indexOf("sunos") >= 0);
+    }
+
+}
